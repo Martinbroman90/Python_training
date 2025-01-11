7c0d6481221a349173daa6c61631d5a3bf7713d0
Martinbroman90 <Martinbroman90@gmail.com>
2025-01-11 15:47:16 +0100

    Decorators "del functions"

--- the_complete_python_developer/The_Complete_Python_Developer.py
+++ the_complete_python_developer/The_Complete_Python_Developer.py
@@ -1,1 +1,21 @@
-name = "calle"
+def hello(func):
+    func()
+
+def greet():
+    print("still here")
+
+a = hello(greet)
+
+print(a)
+
+
+
+
+
+#def hello():
+#    print("Hellllllooooooooo")
+
+#greet = hello
+#del hello
+
+#print(greet())
