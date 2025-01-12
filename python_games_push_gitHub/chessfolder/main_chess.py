d7fb80dd46f6e67174ed045c420e19b0b08f8f7c
Martinbroman90 <Martinbroman90@gmail.com>
2025-01-12 05:17:25 +0100

    creating a chess game with copilot using pycharm

---
+++ python_games_push_gitHub/chessfolder/main_chess.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Chessboard")
+
+# color
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
