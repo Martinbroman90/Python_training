37e634be908897bba5fa74d5bbd6711f9e8de1b2
Martinbroman90 <Martinbroman90@gmail.com>
2025-01-12 01:16:10 +0100

    decorators

--- the_complete_python_developer/The_Complete_Python_Developer.py
+++ the_complete_python_developer/The_Complete_Python_Developer.py
@@ -1,15 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
 def hello(func):
     func()
 
---
+++ the_complete_python_developer/pythonProject1/decorator_new.py
@@ -0,0 +1,17 @@
+def my_decorator(func):
+    def wrap_func():
+        print("***********")
+        func()
+        print("******************")
+    return wrap_func
+
+def hello():
+    print("hellloooooooooo")
+
+
+def bye():
+    print("see you later")
+
+a = my_decorator(hello)
+a()
+
